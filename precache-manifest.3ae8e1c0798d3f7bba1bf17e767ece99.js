53af749b9d2d2b150894259d4b64cda6cbd835b9
Jatarie <31803548+Jatarie@users.noreply.github.com>
2020-08-06 19:00:12 +0100

    Updates

---
+++ precache-manifest.3ae8e1c0798d3f7bba1bf17e767ece99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422e6ab4ac2d8cecb3f463bf289e5ab3",
+    "url": "/lit/index.html"
+  },
+  {
+    "revision": "de92c5f556cdae43334d",
+    "url": "/lit/static/css/main.e25c9c4c.chunk.css"
+  },
+  {
+    "revision": "aad09cb75294c47f553b",
+    "url": "/lit/static/js/2.c6956f8f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lit/static/js/2.c6956f8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de92c5f556cdae43334d",
+    "url": "/lit/static/js/main.884b0d7e.chunk.js"
+  },
+  {
+    "revision": "f09b868298433c84f3dd",
+    "url": "/lit/static/js/runtime-main.d5b74281.js"
+  }
+]);
